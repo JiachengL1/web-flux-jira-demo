089b1d9c9a165e062c8e8dea3c2364e1d44dca4f
JiachengL1 <jiacheng.li@thoughtworks.com>
2022-10-12 14:21:54 +0800

    init project

---
+++ src/main/java/com/example/webfluxjirademo/WebFluxJiraDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxjirademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxJiraDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxJiraDemoApplication.class, args);
+    }
+
+}
