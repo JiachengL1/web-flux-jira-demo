3923e0f7b6645527ea2659b36f17bf09f1d99319
JiachengL1 <jiacheng.li@thoughtworks.com>
2022-10-14 10:38:27 +0800

    feat: create get all issues by boardId endpoint

--- src/main/java/com/example/webfluxjirademo/controller/IssueController.java
+++ src/main/java/com/example/webfluxjirademo/controller/IssueController.java
@@ -1,9 +1,25 @@
 package com.example.webfluxjirademo.controller;
 
+import com.example.webfluxjirademo.domain.issue.Issue;
+import com.example.webfluxjirademo.service.IssueService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/issue")
 public class IssueController {
+
+    private final IssueService issueService;
+
+    public IssueController(IssueService issueService) {
+        this.issueService = issueService;
+    }
+
+    @GetMapping
+    public Flux<Issue> getAllIssues(@RequestParam("boardId") int boardId) {
+        return issueService.findAllIssues(boardId);
+    }
 }
