ef8758e8e4c8aa9c8328ec63ef04c9af837a9ca0
JiachengL1 <jiacheng.li@thoughtworks.com>
2022-10-25 16:10:05 +0800

    fix: add mongoClient setting for authentication

--- src/main/java/com/example/webfluxjirademo/config/MongoConfiguration.java
+++ src/main/java/com/example/webfluxjirademo/config/MongoConfiguration.java
@@ -1,14 +1,33 @@
 package com.example.webfluxjirademo.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
+import java.util.Collections;
+
 @Configuration
+@PropertySource("classpath:application.properties")
 public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+    @Value("${spring.data.mongodb.port}")
+    private Integer port;
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+    @Value("${spring.data.mongodb.password}")
+    private String password;
 
     @Bean
     public MongoClient mongoClient() {
@@ -17,11 +36,20 @@ public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
 
     @Bean
     public ReactiveMongoTemplate reactiveMongoTemplate() {
-        return new ReactiveMongoTemplate(mongoClient(), getDatabaseName());
+        return new ReactiveMongoTemplate(mongoClient(), this.database);
+    }
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder
+                .credential(MongoCredential.createCredential(
+                        this.username, this.database, this.password.toCharArray()))
+                .applyToClusterSettings(settings -> settings.hosts(
+                        Collections.singletonList(new ServerAddress(this.host, this.port))));
     }
 
     @Override
     protected String getDatabaseName() {
-        return "webflux_jira";
+        return this.database;
     }
 }
